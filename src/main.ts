79152852ea6fbeacee19ee19850565d98e65c960
Jordan Ayvazian <jaayvazian@gmail.com>
2019-12-13 15:35:38 -0800

    Removed some default comments and added a WIP handler for gold trading between character and the merchant

--- src/main.ts
+++ src/main.ts
@@ -1,47 +1,45 @@
 load_code(2);
-// Hey there!
-// This is CODE, lets you control your character with code.
-// If you don't know how to code, don't worry, It's easy.
-// Just set attack_mode to true and ENGAGE!
 
-var attack_mode=true
+var attack_mode = true
 
-setInterval(function(){
-    
-	use_hp_or_mp();
-	loot();
+setInterval(function () {
 
-	if(!attack_mode || character.rip || is_moving(character)) return;
+    use_hp_or_mp();
+    loot();
 
-	var target=get_targeted_monster();
-	if(!target)
-	{
-		target=get_nearest_monster({min_xp:100,max_att:120});
-		if(target) change_target(target);
-		else
-		{
-			set_message("No Monsters");
-			return;
-		}
-	}
-	
-	if(!is_in_range(target))
-	{
-		move(
-			character.x+(target.x-character.x)/2,
-			character.y+(target.y-character.y)/2
-			);
-		// Walk half the distance
-	}
-	else if(can_attack(target))
-	{
-		set_message("Attacking");
-		attack(target);
-	}
+    if (!attack_mode || character.rip || is_moving(character)) return;
 
-},1000/4); // Loops every 1/4 seconds.
+    var target = get_targeted_monster();
+    if (!target) {
+        target = get_nearest_monster({ min_xp: 100, max_att: 120 });
+        if (target) change_target(target);
+        else {
+            set_message("No Monsters");
+            return;
+        }
+    }
 
-// Learn Javascript: https://www.codecademy.com/learn/learn-javascript
-// Write your own CODE: https://github.com/kaansoral/adventureland
-// NOTE: If the tab isn't focused, browsers slow down the game
-// NOTE: Use the performance_trick() function as a workaround
+    if (!is_in_range(target)) {
+        move(
+            character.x + (target.x - character.x) / 2,
+            character.y + (target.y - character.y) / 2
+        );
+        // Walk half the distance
+    }
+    else if (can_attack(target)) {
+        set_message("Attacking");
+        attack(target);
+    }
+
+}, 1000 / 4); // Loops every 1/4 seconds.
+character.on("cm", (object) => {
+    if (object.name === "Patreon") {
+        if (object.message === "I want gold") {
+            send_cm("Patreon", { message: "pos", x: parent.character.real_x, y: parent.character.real_y })
+        }
+        else if (object.message === "trade gp") {
+            send_gold("Patreon", character.gold * .90);
+            send_cm("Patreon", "go home");
+        }
+    }
+});
