4539816ac2c779ad9ee05f9c087cdbb28d7b6d98
Jordan Ayvazian <jaayvazian@gmail.com>
2019-12-15 22:09:15 -0800

    Utils: Moved shared functionality  and new /commands
    There was some duplicate on_command, and checks for cm's from only me. Also added chat clear and an inviteall /command

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,15 +1,48 @@
-export function makeButton(name, callback: () => void) {
-    let $ = parent.$;
-    let tlc = $("#topleftcorner");
+import fs from "fs";
+export const characters = ["WarriorJorbo", "WizardJorbo", "FatherJorbo", "Patreon"];
+export function makeButton(name: string, callback: () => void) {
+    const $ = parent.$;
+    const tlc = $("#topleftcorner");
     tlc.find(`#${name}div`).empty();
     $(`#${name}div`).remove()
-    let button = $(`<div id="${name}div"></div>`).html(`<button class="gamebutton" id="${name}">${name}</button>`);
+    const button = $(`<div id="${name}div"></div>`).html(`<button class="gamebutton" id="${name}">${name}</button>`);
     button.appendTo(tlc);
     $(`#${name}div`).click(callback);
 }
 
 export function clearGameLog() {
-    let $ = parent.$;
-    let gamelog = $("#gamelog");
-    gamelog.clear();
+    const $ = parent.$;
+    const gamelog = $("#gamelog");
+    gamelog.empty();
 }
+
+export function clearChat() {
+    const $ = parent.$;
+    const chat = $("#chatlog");
+    chat.empty();
+}
+
+export function is_me(name: string) {    
+    return characters.includes(name);
+}
+
+handle_command = (command, args) => {
+    if (command === "clear") {
+        clearGameLog();
+        game_log("Game Log Cleared")
+    } else if (command === "clearchat") {
+        clearChat();
+        game_log("Chat Cleared")
+    }
+    else if(command === "inviteall"){
+        characters.forEach((value, index) => {
+            send_party_invite(value);13
+        });
+    } 
+};
+on_party_invite = (from: string) => {
+    if (is_me(from)){
+        accept_party_invite(from);
+    }
+}
+
