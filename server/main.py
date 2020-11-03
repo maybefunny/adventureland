0107dc7c640f48cfda2cf4b75da20fb29a771946
sulton <sulton@cukurin.com>
2020-11-03 11:49:37 +0700

    added flask-cors plugin

--- server/main.py
+++ server/main.py
@@ -1,8 +1,10 @@
 #!./.venv/bin/python
 from flask import Flask, request, render_template
+from flask_cors import CORS
 import json
 app = Flask(__name__)
 
+CORS(app)
 data = None
 
 @app.route('/', methods=['GET', 'POST'])
@@ -14,7 +16,7 @@ def index():
     elif request.method == "POST":
         with open("test.json", "w+") as file:
             json.dump(request.json, file);
-        return ""
+        return "yo"
     return None
 
 if __name__ == '__main__':    
